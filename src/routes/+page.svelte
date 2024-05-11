f43adb3fe3a74add309f6ea0eda128feaf0f246d
Anton Mladenov <anton.mladenov@gmail.com>
2024-05-11 15:35:08 -0400

    responsive layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,16 @@
 	 
 	onMount(async () => {
 		createScene(canvas, canvasContainer)
+		window.addEventListener("resize", onWindowResize);
+    
+
+    function onWindowResize() {
+		const style = getComputedStyle(canvasContainer)
+		const [width, height] = [parseInt(style.width), parseInt(style.height)]
+        camera.aspect = width/height;
+         camera.updateProjectionMatrix();
+        renderer.setSize(width,height);
+    }
 	});
 
 	let periods = [
@@ -29,6 +39,7 @@
 	export let state = {
 		'paused':0,
 		'playing':1,
+		'stopped':2,
 	}
 	
 	let simulationState = state.playing;
@@ -37,10 +48,31 @@
 		simulationState = newState
 	}
 	export function toggleState(){
+		if(simulationState == 2){
+			simulationState = state.paused;
+			animate();
+		}
 		simulationState = !simulationState
 	}
+
+	export function stopRestart(){
+		if(simulationState == state.stopped ){
+			restart()
+			toggleState()
+		}
+		else{
+			simulationState = state.stopped;
+		}
+
+	}
+	export function restart(){
+		simulationTime = (new Date()).getTime()
+		speedInput = 1;
+		period = 1;
+		controlTarget = "Sun"
+	}
 	
-	$:speed = speedInput * period; //3600*24*3.65
+	$:speed = speedInput * period;
 	
 	export function setSpeed(newSpeed){
 		speed = newSpeed;
@@ -59,16 +91,6 @@
 		return controlTarget;
 	}
 	
-	let cameraLock = false;
-	export function toggleCameraLock(){
-		// cameraLock = !cameraLock;
-	}
-	
-	let first = true;
-	export function resetFirst(){
-	    first = true;
-	}
-	
 	let scene;
 	let renderer;
 	let camera
@@ -76,9 +98,10 @@
 	
 	const minDistance = 0.1;
 	const maxDistance = 4000;
+
+
 	
 	export const createScene = (canvas, canvasContainer) => {
-	
 		scene = new THREE.Scene();
 		scene.background = new THREE.TextureLoader().load('8k_stars_milky_way.jpg');
 	
@@ -100,6 +123,18 @@
 	
 		animate();
 	  }
+
+
+
+	// addEventListener("resize",() => {
+	// 	if(!canvasContainer) return;
+	// 	const style = getComputedStyle(canvasContainer)
+	// 	const [width, height] = [parseInt(style.width), parseInt(style.height)]
+	// 	camera.aspect = getWidth() / getHeight();
+	// 	camera.updateProjectionMatrix();
+
+	// 	renderer.setSize(window.innerWidth, window.innerHeight);
+	// },false);
 
 	function cameraReset(){
 		camera.position.set( 0, 20, 100 );
@@ -139,22 +174,20 @@
 	// ---------------------------------------------------------------
 	
 	//Mass (10^24 kg)
-	let planets = [
-		{id:"Sun",      imagePath:'2k_sun.jpg',                 a:0.38709843, a0:0.0, e:0.20563661, e0:0.00002123, i:7.0559432, i0:-0.00590158, l:252.25166724, l0:149470.67486623, w:77.45771895, w0:0.15940013, o:48.33961819, o0:-0.12214182, orbitalEccentricity:0,     mass:19890, orbitalPlaneAngle:0,    rotationalAxisAngle:7.25,   color:'yellow',     radius:696340,   dayInHours:588,       orbitDistance:0,        orbitTime:0 }, //
-		{id:"Mercury",  imagePath:'2k_mercury.jpg',             a:0.38709843, a0:0.00000000, e:0.20563661, e0:0.00002123, i:7.0559432, i0:-0.00590158, l:252.25166724, l0:149472.67486623, w:77.45771895, w0:0.15940013, o:48.33961819, o0:-0.12214182,     orbitalEccentricity:0.206, mass:0.330, orbitalPlaneAngle:7,    rotationalAxisAngle:0.1,    color:'gray',       radius:2439.7,  dayInHours:1407.6,    orbitDistance:36.8,     orbitTime:88*24*3600 },
-		{id:"Venus",    imagePath:'2k_venus_atmosphere.jpg',    a:0.72332102, a0:-0.00000026, e:0.00676399, e0:-0.00005107, i:3.39777545, i0:0.00043494, l:181.97909950, l0:58517.81560260, w:131.76755713, w0:0.05679648, o:76.67261496, o0:-0.27274174,   orbitalEccentricity:0.007, mass:4.97,  orbitalPlaneAngle:3.39, rotationalAxisAngle:177,    color:'yellow',     radius:6051.8,  dayInHours:5832.5,    orbitDistance:67.2,     orbitTime:224.7*24*3600 },
-		{id:"Earth",    imagePath:'2k_earth_daymap.jpg',        a:1.00000018, a0:-0.00000003, e:0.01673163, e0:-0.00003661, i:-0.00054346, i0:-0.01337178, l:100.46691572, l0:35999.37306329, w:102.93005885, w0:0.31795260, o:-5.11260389,  o0:-0.24123856, orbitalEccentricity:0.055, mass:5.97,  orbitalPlaneAngle:0,    rotationalAxisAngle:23,     color:'blue',       radius:6371,    dayInHours:23.9,      orbitDistance:93,       orbitTime:365.2*24*3600},
-		{id:"Mars",     imagePath:'2k_mars.jpg',                a:1.52371243,  a0:0.00000097,  e:0.09336511, e0:0.00009149,  i:1.85181869, i0:-0.00724757, l:-4.56813164,  l0:19140.29934243, w:-23.91744784, w0:0.45223625, o:49.71320984,  o0:-0.26852431,  orbitalEccentricity:0.094, mass:0.642, orbitalPlaneAngle:1.85, rotationalAxisAngle:25,     color:'red',        radius:3389.5,  dayInHours:24.6,      orbitDistance:141.6,    orbitTime:687*24*3600},
-		{id:"Jupiter",  imagePath:'2k_jupiter.jpg',             b:-0.00012452, c:0.06064060,  s:-0.35635438, f:38.35125000, a:5.20248019,  a0:-0.00002864, e:0.04853590, e0:0.00018026,  i:1.29861416, i0:-0.00322699, l:34.33479152,  l0:3034.90371757,  w:14.27495244,  w0:0.18199196, o:100.29282654, o0:0.13024619,  orbitalEccentricity:0.049, mass:1898,  orbitalPlaneAngle:1.31, rotationalAxisAngle:3,      color:'orange',     radius:69911,   dayInHours:9.9,       orbitDistance:483.6,    orbitTime:4331*24*3600},
-		{id:"Saturn",   imagePath:'2k_saturn.jpg', ringImagePath:'2k_saturn_ring_alpha.png',              b:0.00025899,  c:-0.13434469, s:0.87320147,  f:38.35125000, a:9.54149883,  a0:-0.00003065, e:0.05550825, e0:-0.00032044, i:2.49424102, i0:0.00451969,  l:50.07571329,  l0:1222.11494724,  w:92.86136063,  w0:0.54179478, o:113.63998702, o0:-0.25015002,  orbitalEccentricity:0.052, mass:568,   orbitalPlaneAngle:2.49, rotationalAxisAngle:27,     color:'brown',      radius:58232, ringRadius:282000,   dayInHours:10.7,      orbitDistance:886.5,    orbitTime:10747*24*3600},
-		{id:"Uranus",   imagePath:'2k_uranus.jpg',              b:0.00058331,  c:-0.97731848, s:0.17689245,  f:7.67025000, a:19.18797948, a0:-0.00020455, e:0.04685740, e0:-0.00001550, i:0.77298127, i0:-0.00180155, l:314.20276625, l0:428.49512595,   w:172.43404441, w0:0.09266985, o:73.96250215,  o0:0.05739699,  orbitalEccentricity:0.047, mass:86.8,  orbitalPlaneAngle:0.77, rotationalAxisAngle:98,     color:'lightblue',  radius:25362,   dayInHours:17.2,      orbitDistance:1783.7,   orbitTime:30589*24*3600},
-		{id:"Neptune",  imagePath:'2k_neptune.jpg',             b:-0.00041348, c:0.68346318,  s:-0.10162547, f:7.67025000, a:30.06952752, a0:0.00006447,  e:0.00895439, e0:0.00000818,  i:1.77005520, i0:0.00022400,  l:304.22289287, l0:218.46515314,   w:46.68158724,  w0:0.01009938, o:131.78635853, o0:-0.00606302,  orbitalEccentricity:0.010, mass:102,   orbitalPlaneAngle:1.77, rotationalAxisAngle:30,     color:'darkblue',   radius:24622,   dayInHours:16.1,      orbitDistance:2795.2,   orbitTime:59800*24*3600},
-	]
-	function getPlanetByID(ID){
-		return planets.filter((p)=>p.id==ID)[0]
+	let planets = {
+		"Sun":{id:"Sun",      imagePath:'2k_sun.jpg',                 a0:0.38709843, a:0.0, e0:0.20563661, e:0.00002123, i0:7.0559432, i:-0.00590158, l0:252.25166724, l:149470.67486623, w0:77.45771895, w:0.15940013, o0:48.33961819, o:-0.12214182, orbitalEccentricity:0,     mass:19890, orbitalPlaneAngle:0,    rotationalAxisAngle:7.25,   color:'yellow',     radius:696340,   dayInHours:588,       orbitDistance:0,        orbitTime:0 }, //
+		"Mercury":{id:"Mercury",  imagePath:'2k_mercury.jpg',             a0:0.38709843, a:0.00000000, e0:0.20563661, e:0.00002123, i0:7.0559432, i:-0.00590158, l0:252.25166724, l:149472.67486623, w0:77.45771895, w:0.15940013, o0:48.33961819, o:-0.12214182,     orbitalEccentricity:0.206, mass:0.330, orbitalPlaneAngle:7,    rotationalAxisAngle:0.1,    color:'gray',       radius:2439.7,  dayInHours:1407.6,    orbitDistance:36.8,     orbitTime:88*24*3600 },
+		"Venus":{id:"Venus",    imagePath:'2k_venus_atmosphere.jpg',    a0:0.72332102, a:-0.00000026, e0:0.00676399, e:-0.00005107, i0:3.39777545, i:0.00043494, l0:181.97909950, l:58517.81560260, w0:131.76755713, w:0.05679648, o0:76.67261496, o:-0.27274174,   orbitalEccentricity:0.007, mass:4.97,  orbitalPlaneAngle:3.39, rotationalAxisAngle:177,    color:'yellow',     radius:6051.8,  dayInHours:5832.5,    orbitDistance:67.2,     orbitTime:224.7*24*3600 },
+		"Earth":{id:"Earth",    imagePath:'2k_earth_daymap.jpg',        a0:1.00000018, a:-0.00000003, e0:0.01673163, e:-0.00003661, i0:-0.00054346, i:-0.01337178, l0:100.46691572, l:35999.37306329, w0:102.93005885, w:0.31795260, o0:-5.11260389,  o:-0.24123856, orbitalEccentricity:0.055, mass:5.97,  orbitalPlaneAngle:0,    rotationalAxisAngle:23,     color:'blue',       radius:6371,    dayInHours:23.9,      orbitDistance:93,       orbitTime:365.2*24*3600},
+		"Mars":{id:"Mars",     imagePath:'2k_mars.jpg',                a0:1.52371243,  a:0.00000097,  e0:0.09336511, e:0.00009149,  i0:1.85181869, i:-0.00724757, l0:-4.56813164,  l:19140.29934243, w0:-23.91744784, w:0.45223625, o0:49.71320984,  o:-0.26852431,  orbitalEccentricity:0.094, mass:0.642, orbitalPlaneAngle:1.85, rotationalAxisAngle:25,     color:'red',        radius:3389.5,  dayInHours:24.6,      orbitDistance:141.6,    orbitTime:687*24*3600},
+		"Jupiter":{id:"Jupiter",  imagePath:'2k_jupiter.jpg',             b:-0.00012452, c:0.06064060,  s:-0.35635438, f:38.35125000, a0:5.20248019,  a:-0.00002864, e0:0.04853590, e:0.00018026,  i0:1.29861416, i:-0.00322699, l0:34.33479152,  l:3034.90371757,  w0:14.27495244,  w:0.18199196, o0:100.29282654, o:0.13024619,  orbitalEccentricity:0.049, mass:1898,  orbitalPlaneAngle:1.31, rotationalAxisAngle:3,      color:'orange',     radius:69911,   dayInHours:9.9,       orbitDistance:483.6,    orbitTime:4331*24*3600},
+		"Saturn":{id:"Saturn",   imagePath:'2k_saturn.jpg', ringImagePath:'2k_saturn_ring_alpha.png',              b:0.00025899,  c:-0.13434469, s:0.87320147,  f:38.35125000, a0:9.54149883,  a:-0.00003065, e0:0.05550825, e:-0.00032044, i0:2.49424102, i:0.00451969,  l0:50.07571329,  l:1222.11494724,  w0:92.86136063,  w:0.54179478, o0:113.63998702, o:-0.25015002,  orbitalEccentricity:0.052, mass:568,   orbitalPlaneAngle:2.49, rotationalAxisAngle:27,     color:'brown',      radius:58232, ringRadius:282000,   dayInHours:10.7,      orbitDistance:886.5,    orbitTime:10747*24*3600},
+		"Uranus":{id:"Uranus",   imagePath:'2k_uranus.jpg',              b:0.00058331,  c:-0.97731848, s:0.17689245,  f:7.67025000, a0:19.18797948, a:-0.00020455, e0:0.04685740, e:-0.00001550, i0:0.77298127, i:-0.00180155, l0:314.20276625, l:428.49512595,   w0:172.43404441, w:0.09266985, o0:73.96250215,  o:0.05739699,  orbitalEccentricity:0.047, mass:86.8,  orbitalPlaneAngle:0.77, rotationalAxisAngle:98,     color:'lightblue',  radius:25362,   dayInHours:17.2,      orbitDistance:1783.7,   orbitTime:30589*24*3600},
+		"Neptune":{id:"Neptune",  imagePath:'2k_neptune.jpg',             b:-0.00041348, c:0.68346318,  s:-0.10162547, f:7.67025000, a0:30.06952752, a:0.00006447,  e0:0.00895439, e:0.00000818,  i0:1.77005520, i:0.00022400,  l0:304.22289287, l:218.46515314,   w0:46.68158724,  w:0.01009938, o0:131.78635853, o:-0.00606302,  orbitalEccentricity:0.010, mass:102,   orbitalPlaneAngle:1.77, rotationalAxisAngle:30,     color:'darkblue',   radius:24622,   dayInHours:16.1,      orbitDistance:2795.2,   orbitTime:59800*24*3600},
 	}
-	export function getPlanets(){
-		return planets;
+	let planetList = Object.values(planets);
+	function getPlanetByID(ID){
+		return planets[ID]
 	}
 	
 	//used to rotate objects to match starting orientation of images and camera, (mostly 2D shapes being maped to 3D)
@@ -162,7 +195,7 @@
 	globalOrientationQuaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI/2); // Axis of rotation is x axis
 	
 	function buildPlanets(){
-		planets.forEach(planet => {
+		planetList.forEach(planet => {
 	
 			planet.mesh = getPlanetMesh(planet);
 			planet.rotationSpeed = getPlanetRotationSpeed(planet);
@@ -249,6 +282,9 @@
 		return points;
 	}
 	
+	//calcPosition designed from equations provided at
+	//https://ssd.jpl.nasa.gov/planets/approx_pos.html
+
 	//takes a planet object conatining necessary coefficients and a unix timestamp in milliseconds and calculates approximate position of the planet
 	function calcPosition(planet, T){ 
 		const time = ((T / 86400000) + 2440587.5 - 2451545.0)/36525 
@@ -264,12 +300,12 @@
 	
 	function calcElements(planet, T = (new Date()).getTime()){
 	
-		planet.A = planet.a + planet.a0 * T;
-		planet.E = planet.e + planet.e0 * T;
-		planet.I = planet.i + planet.i0 * T;
-		planet.L = planet.l + planet.l0 * T;
-		planet.W = planet.w + planet.w0 * T;
-		planet.O = planet.o + planet.o0 * T;
+		planet.A = planet.a0 + planet.a * T;
+		planet.E = planet.e0 + planet.e * T;
+		planet.I = planet.i0 + planet.i * T;
+		planet.L = planet.l0 + planet.l * T;
+		planet.W = planet.w0 + planet.w * T;
+		planet.O = planet.o0 + planet.o * T;
 	
 	}
 	
@@ -372,14 +408,14 @@
 		const simulationTimePassed = clock.getDelta() * 1000 * speed;
 		
 		
-		requestAnimationFrame( animate );
+		if(simulationState!= state.stopped)requestAnimationFrame( animate );
 		controls.update();
 		renderer.render( scene, camera );
 
 		if(simulationState == state.playing) {
 
 			simulationTime += simulationTimePassed;
-			planets.forEach(planet => {
+			planetList.forEach(planet => {
 				const rotation = planet.rotationSpeed * simulationTimePassed;
 				planet.mesh.rotation.y += rotation;
 				if(planet.id=="Sun"){
@@ -396,14 +432,14 @@
 			
 			const targetPosition = getPlanetByID(controlTarget).mesh.position;
 			let scaledTargetPosition = (new THREE.Vector3()).copy(targetPosition).multiplyScalar(-1);
-			planets.forEach(planet=>{
+			planetList.forEach(planet=>{
 				planet.mesh.position.add(scaledTargetPosition)
 				if(planet.id=="Saturn"){
 					planet.ring.position.copy(planet.mesh.position)
 				}
 			})
 			const sunPosition = getPlanetByID('Sun').mesh.position;
-			planets.forEach(planet=>{
+			planetList.forEach(planet=>{
 				planet.line.position.copy(sunPosition);
 			})
 
@@ -421,7 +457,8 @@
 		<div class="header-control-div" style="border-width:thin;">
 			<h5 style="min-width:200px;">{format(simulationTime, 'yyyy-MM-dd HH-mm-ss')}</h5>
 			<div class="speed-div ctrl-div">
-				<button on:click={toggleState} style="min-width:72px;"> {simulationState?"Pause":"Play"}</button>
+				<button on:click={toggleState} style="min-width:72px;"> {simulationState==state.playing?"Pause":"Play"}</button>
+				<button on:click={stopRestart} style={"min-width:72px;"}> {simulationState!=state.stopped?"Stop":"Restart"}</button>
 				<label for="speed">Speed:</label>
 				<input type="range" id="speed" name="speed" step="0.1" min="0" max="1000" bind:value={speedInput}/>
 				<input type="number" bind:value={speedInput}>
@@ -437,7 +474,7 @@
 			<div class="target-div ctrl-div">
 				<label for="target">Target</label>
 				<select id="target" name="target" class="select-ctrl" bind:value={controlTarget}>
-					{#each planets as planet}
+					{#each Object.values(planets) as planet}
 						<option value={planet.id}>
 							{planet.id}
 						</option>
